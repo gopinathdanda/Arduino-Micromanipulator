8dbd7e003978154e1802985c355b4aba7db5df1c
Gopinath Danda <gopinath.danda@gmail.com>
2015-05-29 18:05:54 -0400

    reading joystick and moving motor

--- main_manipulator/main_manipulator.ino
+++ main_manipulator/main_manipulator.ino
@@ -1,98 +1,68 @@
-#include <Servo.h>
+/*
+  Arduino Micromanipulator
+  
+  Controls a micromanipulator stage using Arduino using
+  a joystick shield.
+  
+  Hardware:
+  See accompanying image
+  
+  by Gopinath Danda
+  credits Tom Igoe for JoystickMouseControl code
 
-Servo xServo;
-Servo yServo;
-Servo zServo;
+  this code is in the public domain
+  
+*/
 
-// set pin numbers for switch, joystick axes, and LED:
-const int switchPin = 2;      // switch to turn on and off mouse control
-const int mouseButton = 3;    // input pin for the mouse pushButton
-const int xAxis = A0;         // joystick X axis  
-const int yAxis = A1;         // joystick Y axis
-const int ledPin = 5;         // Mouse control LED
+const int xMot1 = 2;
+const int xMot2 = 3;
 
-// parameters for reading the joystick:
-int range = 12;               // output range of X or Y movement
-int responseDelay = 5;        // response delay of the mouse, in ms
-int threshold = range/4;      // resting threshold
-int center = range/2;         // resting position value
+const int yMot1 = 4;
+const int yMot2 = 5;
 
-boolean mouseIsActive = false;    // whether or not to control the mouse
-int lastSwitchState = LOW;        // previous switch state
+int range = 12;
+int reponseDelay = 5;
+int threshold = range/4;
+int center = range/2;
 
-void setup() {
-  pinMode(switchPin, INPUT);       // the switch pin
-  pinMode(ledPin, OUTPUT);         // the LED pin 
-  xServo.attach(9);
-  xServo.write(90);
-  yServo.attach(10);
-  yServo.write(90);
-  zServo.attach(11);
-  zServo.write(90);
+//boolean IsActive = false;
+//int lastSwitchState = LOW;
+
+void setup(){
+  pinMode(xMot1, OUTPUT);
+  pinMode(xMot2, OUTPUT);
+  pinMode(yMot1, OUTPUT);
+  pinMode(yMot2, OUTPUT);
 }
 
-void loop() {
-  // read the switch:
-  int switchState = digitalRead(switchPin);
-  // if it's changed and it's high, toggle the mouse state:
-  if (switchState != lastSwitchState) {
-    if (switchState == HIGH) {
-      mouseIsActive = !mouseIsActive;
-      // turn on LED to indicate mouse state:
-      digitalWrite(ledPin, mouseIsActive);
-    } 
-  }
-  // save switch state for next comparison:
-  lastSwitchState = switchState;
-
-  // read and scale the two axes:
+void loop(){
   int xReading = readAxis(A0);
   int yReading = readAxis(A1);
-
-  // if the mouse control state is active, move the mouse:
-  if (mouseIsActive) {
-    Mouse.move(xReading, yReading, 0);
-  }  
-
-  // read the mouse button and click or not click:
-  // if the mouse button is pressed:
-  if (digitalRead(mouseButton) == HIGH) {
-    // if the mouse is not pressed, press it:
-    if (!Mouse.isPressed(MOUSE_LEFT)) {
-      Mouse.press(MOUSE_LEFT);
-    }
-  } 
-  // else the mouse button is not pressed:
-  else {
-    // if the mouse is pressed, release it:
-    if (Mouse.isPressed(MOUSE_LEFT)) {
-      Mouse.release(MOUSE_LEFT); 
-    }
-  }
-
-  delay(responseDelay);
+  
+  moveMotor(xReading, xMot1, xMot2);
+  moveMotor(yReading, yMot1, yMot2);
+  
 }
 
-/*
-  reads an axis (0 or 1 for x or y) and scales the 
- analog input range to a range from 0 to <range>
- */
-
-int readAxis(int thisAxis) { 
-  // read the analog input:
+int readAxis(int thisAxis){
   int reading = analogRead(thisAxis);
-
-  // map the reading from the analog input range to the output range:
   reading = map(reading, 0, 1023, 0, range);
-
-  // if the output reading is outside from the
-  // rest position threshold,  use it:
   int distance = reading - center;
-
-  if (abs(distance) < threshold) {
+  if(abs(distance) < threshold){
     distance = 0;
-  } 
-
-  // return the distance for this axis:
+  }
   return distance;
+}
+
+void moveMotor(int reading, int mot1, int mot2){
+  if(reading>threshold){
+    digitalWrite(mot1, true);
+    digitalWrite(mot2, false);
+  }else if(reading<-threshold){
+    digitalWrite(mot1, false);
+    digitalWrite(mot2, true);
+  }else{
+    digitalWrite(mot1, false);
+    digitalWrite(mot2, false);
+  }
 }
